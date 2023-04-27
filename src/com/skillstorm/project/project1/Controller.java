c79b88a5a7541ad27c423fb03dfd1cda049f582e
acarro84 <123746711+acarro84@users.noreply.github.com>
2023-04-27 07:17:27 -0500

    Final v.1

--- src/com/skillstorm/project/project1/Controller.java
+++ src/com/skillstorm/project/project1/Controller.java
@@ -3,7 +3,7 @@ package com.skillstorm.project.project1;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
-import java.util.Random;
+
 
 //import com.skillstorm.project.project1.Scene;
 //import com.skillstorm.project.project1.Player;
@@ -15,32 +15,35 @@ public class Controller {
 
 	}
 
-	Scene awakening = new Scene(1, Scene.awakening,"", "", "");
+	Scene intro1 = new Scene(Scene.awakening,"123", "321", "132");
+	
+	Scene intro2 = new Scene( Scene.introduction, "123", "321", "132");
 
-	Scene introduction = new Scene(2, Scene.introduction, "", "", "");
-
-	Scene battle1 = new Scene(3, Scene.deathMountain, Scene.deathMountainBattlePrompt, Scene.deathMountainWin,
+	Scene battle1 = new Scene(Scene.deathMountain, Scene.deathMountainBattlePrompt, Scene.deathMountainWin,
 			Scene.battleLose);
-	Scene battle2 = new Scene(4, Scene.zorasDomain, Scene.zorasDomainBattlePrompt, Scene.zorasDomainWin,
+	Scene battle2 = new Scene(Scene.zorasDomain, Scene.zorasDomainBattlePrompt, Scene.zorasDomainWin,
 			Scene.battleLose);
-	Scene battle3 = new Scene(5, Scene.sacredRealm, Scene.sacredRealmBattlePrompt, Scene.sacredRealmWin,
+	Scene battle3 = new Scene(Scene.sacredRealm, Scene.sacredRealmBattlePrompt, Scene.sacredRealmWin,
 			Scene.battleLose);
-	Scene battle4 = new Scene(6, Scene.templeOfTime, Scene.templeOfTimeBattlePrompt, Scene.templeOfTimeWin,
+	Scene battle4 = new Scene(Scene.templeOfTime, Scene.templeOfTimeBattlePrompt, Scene.templeOfTimeWin,
 			Scene.battleLose);
-	Scene finalBattle = new Scene(7, Scene.ganonsCastle, "", Scene.rescueZelda, Scene.battleLose);
+	Scene finalBattle = new Scene(Scene.ganonsCastle, Scene.ganonBattlePrompt, Scene.rescueZelda, Scene.battleLose);
 
 	public void runGame() {
-		Scene currentScene = new Scene();
-		currentScene = awakening;
+		Scene currentScene = intro1;
+//		currentScene = intro1;
 //		System.out.println();
 		System.out.println(currentScene.getSceneSetup());
+		System.out.println("==============================================================================================");
 		Player player1 = new Player();
-		player1.setHearts(5);
+		player1.setHearts(3);
 		System.out.println("Hello, hero. Do you remember your name? \nEnter your name:");
 		Scanner in = new Scanner(System.in);
 		player1.setName(in.nextLine());
-		currentScene = introduction;
+		currentScene = intro2;
+		System.out.println("==============================================================================================");
 		System.out.println(player1.getName() + currentScene.getSceneSetup());
+		System.out.println("==============================================================================================");
 		System.out.println(
 				"First, let's refresh your memory of your fighting style.\n1. A strength-based Warrior weilding a sword and shield.\n2. A magic-based Mage with a magic infused staff. \n3. A nimble, dexterity-based Archer who specialized in bows. \nEnter 1, 2, or 3.");
 
@@ -61,6 +64,7 @@ public class Controller {
 
 //		}
 		System.out.println(player1);
+		System.out.println("==============================================================================================");
 
 		ArrayList<Scene> mainScenes = new ArrayList<>(4);
 		mainScenes.add(battle1);
@@ -73,17 +77,21 @@ public class Controller {
 			currentScene = current;
 			if (player1.getHearts() > 0) {
 				System.out.println(currentScene.getSceneSetup());
+				System.out.println("==============================================================================================");
 				System.out.println(currentScene.getBattlePrompt());
 				int battleSelection = in.nextInt();
 				if (battleSelection == 1)
 					if (player1.getStrength() < 10) {
+						System.out.println("==============================================================================================");
 						System.out.println(currentScene.getBattleResultLose());
 						player1.setHearts(player1.getHearts() - 1);
 					} else {
+						System.out.println("==============================================================================================");
 						System.out.println(currentScene.getBattleResultWin());
 					}
 				if (battleSelection == 2)
 					if (player1.getMagic() < 10) {
+						System.out.println("==============================================================================================");
 						System.out.println(currentScene.getBattleResultLose());
 						player1.setHearts(player1.getHearts() - 1);
 					} else {
@@ -91,9 +99,11 @@ public class Controller {
 					}
 				if (battleSelection == 3)
 					if (player1.getDexterity() < 10) {
+						System.out.println("==============================================================================================");
 						System.out.println(currentScene.getBattleResultLose());
 						player1.setHearts(player1.getHearts() - 1);
 					} else {
+						System.out.println("==============================================================================================");
 						System.out.println(currentScene.getBattleResultWin());
 					}
 			} else
@@ -101,16 +111,22 @@ public class Controller {
 		}
 		if (player1.getHearts() > 0) {
 			currentScene = finalBattle;
+			System.out.println("==============================================================================================");
 			System.out.println(currentScene.getSceneSetup());
+			System.out.println("==============================================================================================");
 			System.out.println(currentScene.getBattlePrompt());
 			int bossSelection = in.nextInt();
-			if (bossSelection == 1) {
+			if (bossSelection == 2) {
+				System.out.println("==============================================================================================");
 				System.out.println(currentScene.getBattleResultWin());
 				System.out.println();
 			} else {
-				System.out.println();
+				System.out.println("==============================================================================================");
+				System.out.println("You have failed to save the Princess. Try again to save her and the land of Hyrule.\nGAME OVER");
 			}
 
+		} else {
+			System.out.println("///////////////////////////////////////////\nSorry, you have run out of hearts. Try again to save the Princess.\nGAME OVER");
 		}
 
 		in.close();
